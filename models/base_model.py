5ed3d2bfa12b0118e7ae2a67bcc0233d5cedcfdc
Ilayoub <jrindou.ayoub@gmail.com>
2024-05-21 13:36:55 -0700

    models

--- models/base_model.py
+++ models/base_model.py
@@ -1,56 +1,50 @@
 #!/usr/bin/python3
-"""This script is the base model"""
 
-import uuid
+"""Defines BaseModel class"""
+
+from uuid import uuid4
+import models
 from datetime import datetime
-from models import storage
 
 
 class BaseModel:
-
-    """Class from which all other classes will inherit"""
-
-    def __init__(self, *args, **kwargs):
-        """Initializes instance attributes
+    """
+        BaseModel class define common attributes/methods for other classes
 
         Args:
-            - *args: list of arguments
-            - **kwargs: dict of key-values arguments
-        """
+            *args: Not used.
+            **kwargs: A dict of attributes.
+    """
 
-        if kwargs is not None and kwargs != {}:
-            for key in kwargs:
-                if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                else:
-                    self.__dict__[key] = kwargs[key]
+    def __init__(self, *args, **kwargs):
+        """Initialize new BaseModel"""
+        if kwargs:
+            for key, val in kwargs.items():
+                if key != "__class__":
+                    if key in ["created_at", "updated_at"]:
+                        val = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, val)
         else:
-            self.id = str(uuid.uuid4())
+            self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
-
-    def __str__(self):
-        """Returns official string representation"""
-
-        return "[{}] ({}) {}".\
-            format(type(self).__name__, self.id, self.__dict__)
+            models.storage.new(self)
 
     def save(self):
-        """updates the public instance attribute updated_at"""
-
+        """updates the pub instance updated_at with current datetime"""
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
+
+    def __str__(self):
+        """string representation of a BaseModel instance"""
+        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__"""
-
-        my_dict = self.__dict__.copy()
-        my_dict["__class__"] = type(self).__name__
-        my_dict["created_at"] = my_dict["created_at"].isoformat()
-        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
-        return my_dict
+        """returns a dict containing all keys/values of __dict__ of instance"""
+        _dict = {
+            key: val if key not in ["created_at", "updated_at"] else
+            val.strftime("%Y-%m-%dT%H:%M:%S.%f")
+            for key, val in self.__dict__.items()
+        }
+        _dict['__class__'] = self.__class__.__name__
+        return _dict
