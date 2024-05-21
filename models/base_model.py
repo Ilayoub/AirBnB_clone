d1bceaa0863004c6b9a9c84203cf799392648db9
Ilayoub <jrindou.ayoub@gmail.com>
2024-05-21 13:38:27 -0700

    models

--- models/base_model.py
+++ models/base_model.py
@@ -1,50 +1,51 @@
 #!/usr/bin/python3
-
-"""Defines BaseModel class"""
-
-from uuid import uuid4
+"""Defines the BaseModel class."""
 import models
+from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel:
-    """
-        BaseModel class define common attributes/methods for other classes
-
-        Args:
-            *args: Not used.
-            **kwargs: A dict of attributes.
-    """
+    """Represents the BaseModel of the HBnB project."""
 
     def __init__(self, *args, **kwargs):
-        """Initialize new BaseModel"""
-        if kwargs:
-            for key, val in kwargs.items():
-                if key != "__class__":
-                    if key in ["created_at", "updated_at"]:
-                        val = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, key, val)
+        """Initialize a new BaseModel.
+
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
         else:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def save(self):
-        """updates the pub instance updated_at with current datetime"""
-        self.updated_at = datetime.now()
+        """Update updated_at with the current datetime."""
+        self.updated_at = datetime.today()
         models.storage.save()
 
-    def __str__(self):
-        """string representation of a BaseModel instance"""
-        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
-
     def to_dict(self):
-        """returns a dict containing all keys/values of __dict__ of instance"""
-        _dict = {
-            key: val if key not in ["created_at", "updated_at"] else
-            val.strftime("%Y-%m-%dT%H:%M:%S.%f")
-            for key, val in self.__dict__.items()
-        }
-        _dict['__class__'] = self.__class__.__name__
-        return _dict
+        """Return the dictionary of the BaseModel instance.
+
+        Includes the key/value pair __class__ representing
+        the class name of the object.
+        """
+        rdict = self.__dict__.copy()
+        rdict["created_at"] = self.created_at.isoformat()
+        rdict["updated_at"] = self.updated_at.isoformat()
+        rdict["__class__"] = self.__class__.__name__
+        return rdict
+
+    def __str__(self):
+        """Return the print/str representation of the BaseModel instance."""
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
