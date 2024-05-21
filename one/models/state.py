a5c906344041e88991bd8bd13b96687e401e85aa
Ilayoub <jrindou.ayoub@gmail.com>
2024-05-21 16:40:52 -0700

    let us test

---
+++ one/models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""The module creates a UserClass"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Class for managing state objects"""
+
+    name = ""
